34b469a8f4e36274daccdbb407cde237dcd31f95
John <johnnam31@gmail.com>
2015-03-31 23:45:43 +0900

    1. DB 리셋팅 후 연동 2. postingDAO 기능 안정

--- src/com/bac/posting/PostingDao.java
+++ src/com/bac/posting/PostingDao.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class PostingDao {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://54.64.160.105:3306/AYH";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/AYH";
 
 	static final String USER = "root";
 	static final String PASS = "900418";
@@ -44,7 +44,12 @@ public class PostingDao {
 			conn = getConnection();
 			stmt = conn.createStatement();
 			
-			String sql ="Select * from posting";
+			String sql ="SELECT A.*, B.thumb, C.posting_seq, IFNULL(AVG(C.point), 2.5) AS avg "+
+						"FROM posting AS A "+
+						"LEFT OUTER JOIN user B ON B.id = A.writer "+ 
+						"LEFT OUTER JOIN comment C ON C.posting_seq = A.seq "+ 
+						"GROUP BY A.seq "+
+						"ORDER BY A.seq DESC";
 			ResultSet rs = stmt.executeQuery(sql);
 			
 			while(rs.next()){
@@ -53,8 +58,95 @@ public class PostingDao {
 				item.put("content", rs.getString("content"));
 				item.put("writer", rs.getString("writer"));
 				item.put("regdate", rs.getString("regdate"));
-				//item.put("thumb", rs.getString("thumb"));
-				//item.put("avg", rs.getString("avg"));
+				item.put("thumb", rs.getString("thumb"));
+				item.put("avg", rs.getString("avg"));
+				
+				result.add(item);
+			}
+
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		}catch(SQLException se){
+			se.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+		}
+
+		return result;
+	}
+	//writer=id
+	public List<HashMap<String, Object>> getUserPosting(String id) {
+		Connection conn = null;
+		Statement stmt = null;
+		List<HashMap<String, Object>> result = new ArrayList<HashMap<String, Object>>();
+
+		try{
+			conn = getConnection();
+			stmt = conn.createStatement();
+			
+			String sql ="SELECT A.*, B.thumb, C.posting_seq, IFNULL(AVG(C.point), 2.5) AS avg "+
+						"FROM posting AS A "+
+						"LEFT OUTER JOIN user B ON B.id = A.writer "+
+						"LEFT OUTER JOIN comment C ON C.posting_seq = A.seq "+
+						"where A.writer='"+id+"' "+ 
+						"GROUP BY A.seq "+ 
+						"ORDER BY A.regdate DESC";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()){
+				HashMap<String, Object> item = new HashMap<String, Object>();
+				item.put("seq", rs.getString("seq"));
+				item.put("content", rs.getString("content"));
+				item.put("writer", rs.getString("writer"));
+				item.put("regdate", rs.getString("regdate"));
+				item.put("thumb", rs.getString("thumb"));
+				item.put("avg", rs.getString("avg"));
+				
+				result.add(item);
+			}
+
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		}catch(SQLException se){
+			se.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+		}
+
+		return result;
+	}
+	public List<HashMap<String, Object>> getUserCommentPosting(String id) {
+		Connection conn = null;
+		Statement stmt = null;
+		List<HashMap<String, Object>> result = new ArrayList<HashMap<String, Object>>();
+
+		try{
+			conn = getConnection();
+			stmt = conn.createStatement();
+			
+			String sql ="SELECT A.*, B.thumb, C.posting_seq, IFNULL(AVG(C.point), 2.5) AS avg "+
+						"FROM posting AS A "+
+						"LEFT OUTER JOIN user B ON B.id = A.writer "+
+						"LEFT OUTER JOIN comment C ON C.posting_seq = A.seq "+
+						"where C.writer='"+id+"' "+ 
+						"GROUP BY A.seq "+ 
+						"ORDER BY A.regdate DESC";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()){
+				HashMap<String, Object> item = new HashMap<String, Object>();
+				item.put("seq", rs.getString("seq"));
+				item.put("content", rs.getString("content"));
+				item.put("writer", rs.getString("writer"));
+				item.put("regdate", rs.getString("regdate"));
+				item.put("thumb", rs.getString("thumb"));
+				item.put("avg", rs.getString("avg"));
 				
 				result.add(item);
 			}
@@ -73,7 +165,49 @@ public class PostingDao {
 		return result;
 	}
 	
+	public List<HashMap<String, Object>> getPopularPosting() {
+		Connection conn = null;
+		Statement stmt = null;
+		List<HashMap<String, Object>> result = new ArrayList<HashMap<String, Object>>();
 
+		try{
+			conn = getConnection();
+			stmt = conn.createStatement();
+			
+			String sql ="SELECT A.*, B.thumb, C.posting_seq, IFNULL(AVG(C.point), 2.5) AS avg "+
+						"FROM posting AS A "+
+						"LEFT OUTER JOIN user B ON B.id = A.writer "+
+						"INNER JOIN comment C ON C.posting_seq = A.seq "+
+						"GROUP BY A.seq "+
+						"ORDER BY avg DESC";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()){
+				HashMap<String, Object> item = new HashMap<String, Object>();
+				item.put("seq", rs.getString("seq"));
+				item.put("content", rs.getString("content"));
+				item.put("writer", rs.getString("writer"));
+				item.put("regdate", rs.getString("regdate"));
+				item.put("thumb", rs.getString("thumb"));
+				item.put("avg", rs.getString("avg"));
+				
+				result.add(item);
+			}
+
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		}catch(SQLException se){
+			se.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+		}
+
+		return result;
+	}
+	
 	public String postPosting(Map<String, String[]> postingParam){
 		Connection conn = null;
 		Statement stmt = null;
@@ -158,7 +292,5 @@ public class PostingDao {
 		return result;
 	}
 	
-	public void postImg(){
-		MultipartRequest multi = new MultipartRequest(request, savePath, sizeLimit,"euc-kr",  new DefaultFileRenamePolicy());
-	}
+
 }//end FirstExample
