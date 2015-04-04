c08b2a926f99d3a21e03a051989ac0f02da54db9
John <johnnam31@gmail.com>
2015-04-04 13:24:06 +0900

    DB 변

--- src/common/hungry/comment/CommentDao.java
+++ src/common/hungry/comment/CommentDao.java
@@ -18,7 +18,7 @@ public class CommentDao {
 //	static final String PASS = "900418";
 	
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://localhost:3306/AYH";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/CD";
 
 	static final String USER = "root";
 	static final String PASS = "900418";
